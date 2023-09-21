f3dcb4dc26d4e9194c3f127f242b88b40a52e1b1
p05865 <Beka.Mskhvilidze@fricke.de>
2023-09-21 09:07:33 +0200

    Delete unnötige Files

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        com.company.XMLReadWriter xmlReadWriter = new com.company.XMLReadWriter();
+        File file2 = new File(getReplaceWhitespaces(args[1]));
+        File file = new File(getReplaceWhitespaces(args[0]));
+        if (!file2.exists()) {
+            throw new IllegalArgumentException("Folder existiert nicht!");
+        } else {
+            File[] files = file2.listFiles();
+            if (files != null) {
+                if (files.length > 50000) {
+                    xmlReadWriter.createForXMLFile(files, file.getPath());
+                } else if (files.length < 50000 && files.length > 25000) {
+                    xmlReadWriter.createTwoXMLFile(files, file.getPath());
+                } else {
+                    xmlReadWriter.createOneXMLFile(files, file.getPath());
+                }
+            } else {
+                throw new IllegalArgumentException("Array ist leer");
+            }
+        }
+
+        for (Thread thread : xmlReadWriter.getList()) {
+            thread.join();
+        }
+
+        System.out.println("AAA");
+    }
+
+    private static String getReplaceWhitespaces(String path) {
+        String[] basketForPath = path.split("SAP");
+        return basketForPath[0].replaceAll("\\s+", "  ") + "SAP" + basketForPath[1];
+    }
+}
--- src/main/java/com/company/Worker.java
+++ src/main/java/com/company/Worker.java
@@ -11,17 +11,44 @@ public class Worker {
 
     public static void main(String[] args) {
 
-        File filePath = new File("\\\\Dezan1vstiddb1\\sap");
+      /*  File filePath = new File("\\\\Dezan1vstiddb1\\sap");
         File[] files = filePath.listFiles();
         for (File f : files) {
             System.out.println(f.getName());
-        }
+        }*/
+        deleteAllFiles();
     }
     private static String getDate() {
         SimpleDateFormat formatter = new SimpleDateFormat(
                 "ddMMyyyy_HHmmss ");
         Date currentTime = new Date();
         return formatter.format(currentTime);
+    }
+
+    public static void deleteAllFiles() {
+        File file = new File("C:\\Users\\p05865\\IdeaProjects\\XMLDateien\\xml\\");
+        File[] files = file.listFiles();
+        System.out.println(files.length);
+        Thread[] threads = new Thread[6];
+        int m = files.length / threads.length;
+        for (int i = 0; i < threads.length; i++) {
+            int from = m * i;
+            int to = (i + 1 == threads.length) ? files.length : from + m;
+            threads[i] = new Thread(() -> {
+                for (int k = from; k < to; k++) {
+                    System.out.println(Thread.currentThread().getName() + " Delete: " + files[k].getName());
+                    files[k].delete();
+                }
+            });
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 /*
